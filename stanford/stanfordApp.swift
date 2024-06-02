58ed463b92f9b140b52b0d04a6e162fd213c3828
Anish Shrestha <anish@anzteams.com>
2024-06-02 19:33:07 +0545

    Initial Commit

---
+++ stanford/stanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  stanfordApp.swift
+//  stanford
+//
+//  Created by Anish Shrestha on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct stanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
