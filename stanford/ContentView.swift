06f907b21ec097b0d141b4e6d75be2029413c442
Anish Shrestha <anish@anzteams.com>
2024-06-03 19:40:21 +0545

    added emoji flip feature

--- stanford/ContentView.swift
+++ stanford/ContentView.swift
@@ -8,15 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let emojis = ["🇳🇵","🇳🇿","🇺🇸","🇬🇧","🇹🇭","🇧🇪","🇨🇳","🇮🇳"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("First, Github!")
+    
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
+            ForEach(emojis.indices, id: \.self) { index in
+             CardView(isFaceUp: false, emoji: emojis[index])
+                    
+                    
+                
+            }
+            
         }
+        .foregroundColor(.orange)
         .padding()
     }
+    
+    struct CardView: View {
+        @State var isFaceUp = false
+        let emoji: String
+        var body: some View{
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group{
+                if isFaceUp {
+                    base.fill(.white)
+                        .strokeBorder(lineWidth: 2)
+                        .overlay(
+                            Text(emoji)
+                        )
+                } else {
+                    base.fill()
+                }
+            }
+            .onTapGesture {
+                isFaceUp.toggle()
+                print("tapped")
+            }
+         
+        }
+    }
+    
 }
 
 #Preview {
